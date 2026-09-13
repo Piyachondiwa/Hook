bd7159b3e4d8a82cdcb438931a038cf8fd1cc579
Piyachondiwa <sepyouknow123@gmail.com>
2026-09-14 02:06:11 +0700

    Remove obsolete E interaction code

--- interaction-hold.js
+++
@@ -1,7 +0,0 @@
-// Deprecated compatibility shim. Tree interaction lives in interaction-stable.js.
-(() => {
-  document.querySelectorAll('.treePrompt').forEach(el => {
-    el.hidden = true;
-    el.style.display = 'none';
-  });
-})();
