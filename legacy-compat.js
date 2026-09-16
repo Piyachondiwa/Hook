82308a9aab8fafb1388ffa529039b7634947536a
Piyachondiwa <sepyouknow123@gmail.com>
2026-09-16 21:05:16 +0700

    Expose legacy random helper globally

--- legacy-compat.js
+++ legacy-compat.js
@@ -1,6 +1,5 @@
 /* Moonwood: compatibility helpers for legacy visual passes. */
-(()=>{'use strict';
-if(typeof window.rnd!=='function')window.rnd=(x,y=0)=>{const n=Math.sin((x||0)*12.9898+(y||0)*78.233+91827)*43758.5453;return n-Math.floor(n)};
+var rnd=window.rnd||((x,y=0)=>{const n=Math.sin((x||0)*12.9898+(y||0)*78.233+91827)*43758.5453;return n-Math.floor(n)});
+window.rnd=rnd;
 if(typeof window.treeCanopy!=='function')window.treeCanopy=()=>{};
 if(typeof window.grassland!=='function')window.grassland=()=>{};
-})();
